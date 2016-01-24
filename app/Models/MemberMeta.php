999930cb8d8f3332380aaf8f085f4a4c5f833a01
Todd Nestor <todd.nestor@gmail.com>
2016-01-23 22:49:39 -0800

    now custom attributes and meta can be stored

--- app/Models/MemberMeta.php
+++ app/Models/MemberMeta.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Helpers\SMAuthenticate;
+
 class MemberMeta extends Root
 {
     protected $table = "member_meta";
@@ -13,8 +15,16 @@ class MemberMeta extends Root
 }
 
 MemberMeta::saving( function( $meta_item ) {
-	if( \Input::has('sm_customer_id') )
+	if( \Input::has('sm_customer_id') && !empty( \Input::get('sm_customer_id') ) && \Input::has('key') && !empty( \Input::get('key') ) )
 	{
-		
+		$attribute = CustomAttribute::whereUserId( \Input::get('sm_customer_id') )->whereName( \Input::get('key') )->first();
+
+		if( !$attribute )
+			$attribute = CustomAttribute::create(['user_id' => \Input::get('sm_customer_id'), 'name' => \Input::get('key'), 'type' => \Input::get('type', '')]);
+
+		$meta_item->custom_attribute_id = $attribute->id;
+
+		if( SMAuthenticate::set() )
+			$meta_item->member_id = \Auth::user()->id;
 	}
 } );
