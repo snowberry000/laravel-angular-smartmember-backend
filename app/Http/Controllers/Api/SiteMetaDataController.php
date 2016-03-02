e78d1a6148015551a02aff2c17ae065cf120961f
Todd Nestor <todd.nestor@gmail.com>
2016-03-02 10:24:34 -0800

    fixed spacing

--- app/Http/Controllers/Api/SiteMetaDataController.php
+++ app/Http/Controllers/Api/SiteMetaDataController.php
@@ -235,8 +235,8 @@ class SiteMetaDataController extends SMController
             $site = Site::whereDomain(\Input::get('domain'))->first();
             $data = SiteMetaData::whereSiteId($site->id)->whereIn("key",array('google_analytics_id','facebook_retargetting_pixel','facebook_conversion_pixel','bing_id','google_webmaster_tag','bing_webmaster_tag','active_campaign_id','fb_share_description'))->get();
         }
-		else
-		{
+        else
+        {
             if( isset( $this->site->id ) ) {
                 $data = SiteMetaData::whereSiteId($this->site->id)->whereIn("key", array('google_analytics_id', 'facebook_retargetting_pixel', 'facebook_conversion_pixel', 'bing_id', 'google_webmaster_tag', 'bing_webmaster_tag', 'active_campaign_id', 'fb_share_description'))->get();
             }
