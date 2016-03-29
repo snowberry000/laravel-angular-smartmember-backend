6f6fba2fb8577e833f4eb3647cb54cdc868d9a1b
meanpeace52 <webstar2222@gmail.com>
2016-03-29 13:35:10 -0500

    Revert "An attempt at fixing CORS for Pagoda"
    This reverts commit c79a0f6725d0c3f487fb0a7eacaf60afafc244b0.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,6 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\Cors::class, // Place Cors middleware here
         \App\Http\Middleware\ScriptCheck::class,
         \App\Http\Middleware\AfterGetRequest::class,
     ];
