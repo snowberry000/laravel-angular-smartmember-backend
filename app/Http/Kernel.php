7a093233967f5dc5dab408c06cb6dc97af9d21c3
meanpeace52 <webstar2222@gmail.com>
2016-03-29 11:22:31 -0500

    An attempt at fixing CORS for Pagoda

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\Cors::class, // Place Cors middleware here
         \App\Http\Middleware\ScriptCheck::class,
         \App\Http\Middleware\AfterGetRequest::class,
     ];
